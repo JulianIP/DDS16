5bdd29253ba0b9c09aefc7c4a7f8b84eedb2aed0
marcos caccavaio <marcos caccavaio>
2016-03-23 15:05:43 -0300

    Modifique el app.java porque me tiraba errores

--- src/main/java/JulianIP/Prueba/App.java
+++ src/main/java/JulianIP/Prueba/App.java
@@ -12,11 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Marco 20160322 19.42" );
-<<<<<<< HEAD
         System.out.println ("Juli");
         System.out.println ("Marquitos 1944");
-=======
         System.out.println( "Juli" );
->>>>>>> bd7e05af8487853b3a56eb28757f684d850fbc31
     }
 }
