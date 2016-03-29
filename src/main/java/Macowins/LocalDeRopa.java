fed05f586eca0fc3ed9a472f8654908d36289a1c
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-03-29 17:30:18 -0300

    Realice modificaciones al POM, modificando la version de JUnit

--- src/main/java/Macowins/LocalDeRopa.java
+++ src/main/java/Macowins/LocalDeRopa.java
@@ -6,58 +6,60 @@ public class LocalDeRopa {
 
 	ArrayList<Venta> ventas;
 	double precioFijo;
-	
-	public LocalDeRopa(double valorFijo){
-		precioFijo=valorFijo;
-		ventas= new ArrayList<Venta>();
-		
+
+	public LocalDeRopa(double valorFijo) {
+		precioFijo = valorFijo;
+		ventas = new ArrayList<Venta>();
+
 	}
-	
-	public double precioFinalDe(Prenda unaPrenda  ){
-		double precioFinal= (unaPrenda.getPrecioBase() + precioFijo) * unaPrenda.tasa();
-		return precioFinal; 
+
+	public double precioFinalDe(Prenda unaPrenda) {
+		double precioFinal = (unaPrenda.getPrecioBase() + precioFijo) * unaPrenda.tasa();
+		return precioFinal;
 	}
-	
-	public void vender(int unaFecha, Prenda unaPrenda, int cant){
-		
-		Venta unaVenta=new Venta(unaFecha,unaPrenda,cant);
+
+	public void vender(int unaFecha, Prenda unaPrenda, int cant) {
+
+		Venta unaVenta = new Venta(unaFecha, unaPrenda, cant);
 		ventas.add(unaVenta);
 	}
-	
-	public double gananciaDelDia(int unaFecha){
-		
-		double ganancia=0;
-		
-		for (Venta unaVenta:ventas){
-			if (unaFecha==unaVenta.getFecha()){
-				ganancia+=unaVenta.precioTotal(this);
+
+	public double gananciaDelDia(int unaFecha) {
+
+		double ganancia = 0;
+
+		for (Venta unaVenta : ventas) {
+			if (unaFecha == unaVenta.getFecha()) {
+				ganancia += unaVenta.precioTotal(this);
 			}
 		}
-		
+
 		return ganancia;
-	} 
-	
+	}
+
 	public static void main(String[] args) {
-		
-		Nacional nacional= new Nacional();
-		Internacional internacional= new Internacional();
-		Saco unSaco= new Saco("saco negro",nacional);
-		LocalDeRopa macowins= new LocalDeRopa(100);
-		Saco otroSaco= new Saco("saco gucci", internacional);
-		
-		Camisa unaCamisa=new Camisa("camisa negra", nacional);
-		
-		/*System.out.println(macowins.precioFinalDe(unSaco));
-		System.out.println(macowins.precioFinalDe(otroSaco));
-		System.out.println(macowins.precioFinalDe(unaCamisa));*/
-		
+
+		Nacional nacional = new Nacional();
+		Internacional internacional = new Internacional();
+		Saco unSaco = new Saco("saco negro", nacional);
+		LocalDeRopa macowins = new LocalDeRopa(100);
+		Saco otroSaco = new Saco("saco gucci", internacional);
+
+		Camisa unaCamisa = new Camisa("camisa negra", nacional);
+
+		/*
+		 * System.out.println(macowins.precioFinalDe(unSaco));
+		 * System.out.println(macowins.precioFinalDe(otroSaco));
+		 * System.out.println(macowins.precioFinalDe(unaCamisa));
+		 */
+
 		macowins.vender(20160323, unSaco, 2);
-		
-		macowins.vender(20160325,otroSaco, 1);
-		
+
+		macowins.vender(20160325, otroSaco, 1);
+
 		macowins.vender(20160324, unaCamisa, 1);
-		
-		System.out.println(macowins.gananciaDelDia(20160324));		
-		
+
+		System.out.println(macowins.gananciaDelDia(20160324));
+
 	}
 }
