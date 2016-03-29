bc73f1619982c8849bed4c2dab38142947090a7a
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-03-29 17:28:02 -0300

    Agrego Test que hice

---
+++ test/Macowins/LocalDeRopaTest.java
@@ -0,0 +1,48 @@
+package Macowins;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class LocalDeRopaTest {
+	
+	private Nacional nacional;
+	private Internacional internacional;
+	private LocalDeRopa macowins;
+	private Saco sacoNegro;
+	private Saco sacoGucci;
+	private Camisa camisaNegra;
+	private Pantalon pantalonNegro;
+
+	@Before
+	public void init() {
+		nacional = new Nacional();
+		internacional = new Internacional();
+		macowins = new LocalDeRopa(100);
+		sacoNegro = new Saco("saco negro", nacional);
+		sacoGucci = new Saco("saco gucci", internacional);
+		camisaNegra = new Camisa("camisa negra", nacional);
+		pantalonNegro = new Pantalon("pantalon negro", internacional);
+	}
+
+	@Test
+	public void precioFinalDeSacoNacional() {
+		Assert.assertEquals(400.0, macowins.precioFinalDe(sacoNegro),0.01);
+	}
+	
+	@Test
+	public void precioFinalDeSacoInteracional() {
+		Assert.assertEquals(520.0, macowins.precioFinalDe(sacoGucci),0.01);
+	}
+	
+	@Test
+	public void precioFinalDeCamisaNacional() {
+		Assert.assertEquals(300.0, macowins.precioFinalDe(camisaNegra),0.01);
+	}
+	
+	@Test
+	public void precioFinalDePantalonInternacional() {
+		Assert.assertEquals(455.0, macowins.precioFinalDe(pantalonNegro),0.01);
+	}
+
+}
